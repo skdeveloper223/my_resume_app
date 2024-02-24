92f2f3baa5b41add2d3b64aee04cf4e792452a40
santosh <kolisantosh222@gmail.com>
2024-02-24 12:11:22 +0530

    Profile header design

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_resume_app/profile_screen.dart';
+import 'package:responsive_framework/breakpoint.dart';
+import 'package:responsive_framework/responsive_breakpoints.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,6 +18,15 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+      ),
+      builder: (context, child) => ResponsiveBreakpoints.builder(
+        child: child!,
+        breakpoints: [
+          const Breakpoint(start: 0, end: 450, name: MOBILE),
+          const Breakpoint(start: 451, end: 800, name: TABLET),
+          const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+        ],
       ),
       home: const ProfileScreen(),
     );
--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -10,6 +10,68 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 40,
+                backgroundColor: Colors.blueAccent,
+                child: FlutterLogo(),
+              ),
+              Text("Santosh Koli"),
+              Text("Flutter Developer"),
+              Row(
+                children: [
+                  Icon(Icons.phone),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Mobile No.: "),
+                      Text("+91 9998557234"),
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.mail),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Email Id.: "),
+                      Text("kolisantosh222@gmail.com"),
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.web),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Linkedin : "),
+                      Text("linkedin.com/in/santosh-koli-140219159"),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
